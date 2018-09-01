c70806f262e885a302a36c666a2fffbf5633caf6
guolei.sgl <guolei.sgl@antfin.com>
2018-09-01 21:15:37 +0800

    clean module

--- bridge-serialization/bridge-serialization-jdk/src/main/java/com/glmapper/bridge/serialization/jdk/package-info.java
+++
@@ -1,7 +0,0 @@
-/**
- * serialization jdk implement
- *
- * @author: Jerry
- * @date: 2018/6/28
- */
-package com.glmapper.bridge.serialization.jdk;
--- bridge-transport/bridge-transport-api/src/main/java/com/glmapper/bridge/transport/package-info.java
+++
@@ -1,7 +0,0 @@
-/**
- * transport
- *
- * @author: Jerry
- * @date: 2018/6/28
- */
-package com.glmapper.bridge.transport;
